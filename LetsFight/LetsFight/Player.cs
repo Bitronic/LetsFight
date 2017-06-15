357d7029274727dc405fb44c7df2c4159b52529f
Bitronic <b.adams@gmx.ch>
2017-06-15 21:44:56 +0200

    Init Commit
    Bestehender Code commited

---
+++ LetsFight/LetsFight/Player.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsFight
+{
+    class Player
+    {
+        public string Playername;
+        public int Village_counter;
+        public int Offensive_Units;
+        public int Defensive_Units;
+        public int Lost_Villages;
+        public int Captured_Villages;
+    }
+}
---
+++ LetsFight/LetsFight/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LetsFight
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int MAX_RESSOURCES = 10000;
+
+        }
+        public Player createPlayer(bool isBot, int count, string playername = null)
+        {
+            Player player = new Player();
+            player.Playername = playername != null ? playername : "player_" + count ;
+            player.Playername = isBot ? "bot_" + count : player.Playername;     //If player isBot change name
+
+        }
+    }
+}
