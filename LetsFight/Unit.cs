7b72992e36a5559f78f898f7957ee84a4df2123d
Infilonic <Infilonic@users.noreply.github.com>
2017-07-20 17:12:49 +0200

    Modify Unit.cs

--- LetsFight/Unit.cs
+++ LetsFight/Unit.cs
@@ -6,31 +6,35 @@ using System.Threading.Tasks;
 
 namespace LetsFight
 {
-    class Unit
+    public class Unit
     {
-        public Unit_Definition Fighter = new Unit_Definition() { Life = 100, Damage = 50, Orientation = Unit_Definition.UnitOrientation.Offensive, RessourceCosts = 50 };
-        public Unit_Definition Spy = new Unit_Definition() { Life = 30, Damage = 5, Orientation = Unit_Definition.UnitOrientation.Defensive, RessourceCosts = 15 };
-        public Unit_Definition Knight = new Unit_Definition() { Life = 250, Damage = 100, Orientation = Unit_Definition.UnitOrientation.Offensive, RessourceCosts = 150 };
-        public Unit_Definition King = new Unit_Definition() { Life = 20, Damage = 10, Orientation = Unit_Definition.UnitOrientation.Defensive, RessourceCosts = 2000 };
-    }
-    class Unit_Definition
-    {
-        public int Life { get; set; }
-        public int Damage { get; set; }
-        public enum UnitOrientation
+        public enum StrategyType
         {
-            Offensive,
-            Defensive
-        };
-        public UnitOrientation Orientation { get; set; }
-        public bool isOffensive()
-        {
-            return Orientation == UnitOrientation.Offensive;
+            Offensive = 0,
+            Defensive = 1
         }
-        public bool isDefensive()
-        {
-            return Orientation == UnitOrientation.Defensive;
+
+        private Soldier soldierDetails;
+        private uint soldierCount;
+        private string caption;
+        private StrategyType strategy;
+
+        public Unit(string caption, uint soldierCount, StrategyType strategy, Soldier soldierDetails) {
+            this.soldierDetails = soldierDetails;
+            this.soldierCount = soldierCount;
+            this.strategy = strategy;
         }
-        public int RessourceCosts { get; set; }
+
+        public uint RessourceCost {
+            get { return this.soldierDetails.RessourceCost * this.soldierCount; }
+        }
+
+        public uint SoldierCount {
+            get { return this.soldierCount; }
+        }
+
+        public StrategyType Strategy {
+            get { return this.strategy; }
+        }
     }
 }
